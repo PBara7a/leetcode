4b2b5803721120e5e453d85821bcc7dc4923f659
Paulo Barata <pbara7a@gmail.com>
2022-11-23 16:28:26 +0000

    Lowest Common Ancestor of a Binary Search Tree

---
+++ Lowest Common Ancestor of a Binary Search Tree/lowestCommonAncestor.ts
@@ -0,0 +1,29 @@
+// class TreeNode {
+//   val: number;
+//   left: TreeNode | null;
+//   right: TreeNode | null;
+//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.left = left === undefined ? null : left;
+//     this.right = right === undefined ? null : right;
+//   }
+// }
+
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null
+): TreeNode | null {
+  let currentNode = root;
+
+  while (currentNode && p && q) {
+    if (p.val > currentNode.val && q.val > currentNode.val) {
+      currentNode = currentNode.right;
+    } else if (p.val < currentNode.val && q.val < currentNode.val) {
+      currentNode = currentNode.left;
+    } else {
+      return currentNode;
+    }
+  }
+  return null;
+}
