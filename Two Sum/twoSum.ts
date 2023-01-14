85d2e0e8e8ab9bb697968b3fa0a0806e11ad829b
Paulo Barata <pbara7a@gmail.com>
2023-01-14 11:43:08 +0000

    Valid Parentheses, recursive

--- Two Sum/twoSum.ts
+++ Two Sum/twoSum.ts
@@ -18,14 +18,14 @@ function twoSum(nums: number[], target: number): number[] {
 
 // Recursive solution
 function twoSum2(nums: number[], target: number, start=0, end=nums.length-1, numsMap: Record<number, number> = {}): number[] {
-  if (start === end) return []
+  if (start === end) return [];
 
   numsMap[nums[start]] = start;
   const dif: number = target - nums[start+1];
 
   if (dif in numsMap) {
-    return [numsMap[dif], start+1]
+    return [numsMap[dif], start+1];
   }
 
-  return twoSum2(nums, target, start+1, end, numsMap)
+  return twoSum2(nums, target, start+1, end, numsMap);
 }
--- Valid Parentheses/isValid.ts
+++ Valid Parentheses/isValid.ts
@@ -18,3 +18,24 @@ function isValid(s: string): boolean {
   }
   return bracketStack.length === 0;
 }
+
+// Recursive solution
+function isValid2(s: string, start=0, end=s.length, bracketStack: string[] = []): boolean {
+  if (start === end) return bracketStack.length === 0;
+
+  const lastIn: string = bracketStack[bracketStack.length - 1];
+
+  if (s[start] === bracketPairs[lastIn]) {
+      bracketStack.pop();
+  } else {
+      bracketStack.push(s[start]);
+  }
+
+  return isValid2(s, start+1, end, bracketStack)
+}
+
+const bracketPairs: Record<string, string> = {
+  "(": ")",
+  "{": "}",
+  "[": "]",
+};
