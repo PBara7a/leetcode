3e87d590b484aba2745a8c84288f811040a47883
Paulo Barata <pbara7a@gmail.com>
2023-01-14 13:01:50 +0000

    Valid Palindrome, recursive

--- Valid Palindrome/isPalindrome.ts
+++ Valid Palindrome/isPalindrome.ts
@@ -9,3 +9,16 @@ function isPalindrome(s: string): boolean {
 
   return alphanumericStr === reversedStr;
 }
+
+// Recursive solution
+function isPalindrome2(s: string): boolean {
+  const alphanumericStr: string = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+
+  function innerFunc(s: string, left: number, right: number): boolean {
+    if (left >= right) return true;
+    if (s[left] !== s[right]) return false;
+    return innerFunc(s, left+1, right-1);
+  }
+
+  return innerFunc(alphanumericStr, 0, alphanumericStr.length-1)
+}
